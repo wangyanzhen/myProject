ec80995f68fc6e76da5fb46d66197bb8b49ef993
youguqingxiang <758822456@qq.com>
2016-03-20 10:00:40 +0800

    PC端项目

---
+++ json/json.js
@@ -0,0 +1,12 @@
+//var bannerAry = [
+//    {title: "", img: "图片地址", url: "存储的是点击跳转链接的地址"},
+//    {title: "", img: "图片地址", url: "存储的是点击跳转链接的地址"}
+//];
+var bannerAry = [
+    {title: "公益频道", img: "img/game4.jpg", url: "存储的是点击跳转链接的地址",desc:"爱奇艺公益寻亲平台"},
+    {title: "爱频道", img: "img/game5.jpg", url: "存储的是点击跳转链接的地址",desc:"初恋志"},
+    {title: "广告频道", img: "img/game6.jpg", url: "存储的是点击跳转链接的地址",desc:"老牛吃嫩草《温柔的爱》"},
+    {title: "PPS游戏", img: "img/game10.jpg", url: "存储的是点击跳转链接的地址",desc:"60级后击杀BOSS掉落RMB"},
+    {title: "应用商店", img: "img/game11.jpg", url: "存储的是点击跳转链接的地址",desc:"爱奇艺应用商店"},
+    {title: "爱奇艺商城", img: "img/game12.jpg", url: "存储的是点击跳转链接的地址",desc:"爱奇艺公益寻亲平台"}
+];
